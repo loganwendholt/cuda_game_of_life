c6adae37707eac301a13d741da8e1b2113aa1124
Logan Wendholt <lwendholt@gmail.com>
2015-10-06 22:34:35 -0400

    BASIC SERIAL FUNCTIONALITY WORKING. Hacked together basic OpenGL support using https://github.com/tiagomnh/game-of-life as an example. Fixed errors in GoL implementation. Still need to do some serious refactoring on serial version before beginning CUDA phase.

--- life_serial.c
+++ life_serial.c
@@ -4,89 +4,211 @@
 # include <time.h>
 # include <string.h>
 
-char * life_init(int, double, int, int);
-void life_update(char *, int, int);
+# include <GL/gl.h>
+# include <GL/glu.h>
+# include <GL/glut.h>
 
-int main()
+void life_init();
+void life_update();
+
+GLint window_width = 600;
+GLint window_height = 600;
+GLfloat left = 0.0;
+GLfloat right = 1.0;
+GLfloat bottom = 0.0;
+GLfloat top = 1.0;
+GLint game_width = 100;
+GLint game_height = 100;
+
+double pAlive = .20;
+
+char gridA[100*100];
+char gridB[100*100];
+char *grid = gridA;
+char *nextGrid = gridB;
+
+#define WHITE 1.0, 1.0, 1.0
+#define BLACK 0.0, 0.0, 0.0
+
+int mod (int a, int b)
 {
- 
-  char *grid;
-  int rows = 480;
-  int columns = 640;
-  double pAlive = .20;
- 
-  // Initialize array
-  grid = life_init(time(NULL), pAlive, rows, columns);
+   if(b < 0) 
+     return mod(-a, -b);   
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
 
-  while(1==1)
-  {
-    // Update array
-    life_update(grid, rows, columns);
-  }
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	
+    GLfloat xSize = (right - left) / game_width;
+	GLfloat ySize = (top - bottom) / game_height;
+	
+	GLint x,y;	
+
+	glBegin(GL_QUADS);
+	for (x = 0; x < game_width; ++x) {
+		for (y = 0; y < game_height; ++y) {
+            grid[x+y*game_width]?glColor3f(BLACK):glColor3f(WHITE);
+            
+			glVertex2f(    x*xSize+left,    y*ySize+bottom);
+			glVertex2f((x+1)*xSize+left,    y*ySize+bottom);
+			glVertex2f((x+1)*xSize+left,(y+1)*ySize+bottom);
+			glVertex2f(    x*xSize+left,(y+1)*ySize+bottom);
+		}
+	}
+	glEnd();
+    	
+	glFlush();
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+	window_width = w;
+	window_height = h;
+
+	glViewport(0, 0, window_width, window_height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(left, right, bottom, top);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutPostRedisplay();
+}
+
+void update() {
+	char *tempGrid;
+
+    life_update();
+
+	tempGrid = grid;
+	grid = nextGrid;
+	nextGrid = tempGrid;
+
+	glutPostRedisplay();
+    glutTimerFunc(1000 / 1, update, 0);
+}
+
+void graphics_init()
+{
+	
+	glutInitWindowSize(window_width, window_height);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Game of Life");
+	glClearColor(1, 1, 1, 1);
+	
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  graphics_init();
+
+  // Initialize array
+  life_init();
+
+  update();
+  glutMainLoop();
 
   return 0;
 }
 
-char * life_init(int seed, double pAlive, int rows, int columns)
+void life_init()
 {
-  char *grid;
   int i, j;
 
   // Seed the random number generator
-  srand(seed);
+  srand(time(NULL));
 
   // Allocate memory for array
-  grid = ( char * ) malloc ( ( rows ) * ( columns ) * sizeof ( char ) );
+  //grid = ( char * ) malloc ( ( game_height ) * ( game_width ) * sizeof ( char ) );
 
   // initialize array, with each pixel having a probability of pAlive of being set to 1
-  for( i = 0; i < rows; i++ )
+  for( i = 0; i < game_height; i++ )
   {
-    for( j = 0; j < columns; j++ )
+    for( j = 0; j < game_width; j++ )
     {
-	  if( rand() <  pAlive * ((double)RAND_MAX + 1.0) )
-      {
-        grid[i*columns+j] = 1;
-      }
-      else
-      {
-        grid[i*columns+j] = 0;
-      }
+	 // if( rand() <  pAlive * ((double)RAND_MAX + 1.0) )
+     //{
+     //   grid[i*game_width+j] = 1;
+     // }
+     // else
+     // {
+        gridA[i*game_width+j] = 0;
+		gridB[i*game_width+j] = 0;
+     // }
     }
   }
 
-  return grid;
+i = 2;
+j = 1;
+grid[i*game_width+j] = 1;
+
+i = 1;
+j = 2;
+grid[i*game_width+j] = 1;
+
+i = 0;
+j = 0;
+grid[i*game_width+j] = 1;
+
+i = 0;
+j = 1;
+grid[i*game_width+j] = 1;
+
+i = 0;
+j = 2;
+grid[i*game_width+j] = 1;
+
+  //return grid;
 }
 
-void life_update(char *grid, int rows, int columns)
+void life_update()
 {
   int i,j,aliveCount;
-  for( i = 0; i < rows; i++ )
+  for( i = 0; i < game_height; i++ )
   {
-    for( j = 0; j < columns; j++ )
+    for( j = 0; j < game_width; j++ )
     { 
-	  aliveCount =
-	    grid[ ((i-1) % rows)*columns + ((j-1) % columns) ] +
-        grid[ ((i)   % rows)*columns + ((j-1) % columns) ] +
-        grid[ ((i+1) % rows)*columns + ((j-1) % columns) ] + 
-	    grid[ ((i-1) % rows)*columns + ((j)   % columns) ] +
-        grid[ ((i+1) % rows)*columns + ((j)   % columns) ] +
-	    grid[ ((i-1) % rows)*columns + ((j-1) % columns) ] +
-        grid[ ((i)   % rows)*columns + ((j-1) % columns) ] +
-        grid[ ((i+1) % rows)*columns + ((j-1) % columns) ]; 
+      aliveCount = 0;
+      aliveCount =
+	    grid[ mod((i-1), game_height)*game_width + mod((j-1),  game_width) ] +
+        grid[ mod((i),   game_height)*game_width + mod((j-1),  game_width) ] +
+        grid[ mod((i+1), game_height)*game_width + mod((j-1),  game_width) ] + 
+	    grid[ mod((i-1), game_height)*game_width + mod((j),    game_width) ] +
+        grid[ mod((i+1), game_height)*game_width + mod((j),    game_width) ] +
+	    grid[ mod((i-1), game_height)*game_width + mod((j+1),  game_width) ] +
+        grid[ mod((i),   game_height)*game_width + mod((j+1),  game_width) ] +
+        grid[ mod((i+1), game_height)*game_width + mod((j+1),  game_width) ]; 
 
-      if( grid[i*columns+j] == 1 )
+		if(aliveCount > 0)
+			printf("i: %d, j: %d, count: %d\n", i,j,aliveCount);
+
+		if(grid[i * game_width + j] > 0)
+			printf("grid[%d][%d]: %d\n", i,j,grid[i * game_width + j]);
+
+
+      if( grid[i*game_width+j] == 1 )
       {
         if(aliveCount < 2 || aliveCount > 3)
-          grid[i*columns+j] = 0;
+          nextGrid[i*game_width+j] = 0;
         else
-          grid[i*columns+j] = 1;
+          nextGrid[i*game_width+j] = 1;
       }
       else
       {
         if(aliveCount == 3)
-          grid[i*columns+j] = 1;
+          nextGrid[i*game_width+j] = 1;
         else
-          grid[i*columns+j] = 0;
+          nextGrid[i*game_width+j] = 0;
       }
     }
   }
